774da31e7441e486f62d353af8d7ff8c2c078664
sihon321 <sihon321@daum.net>
2021-04-20 00:38:53 +0900

    [feature] Swift Package Manager
    - dependencies 추가

--- Package.swift
+++ Package.swift
@@ -14,14 +14,15 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "5.2.0")),
-        .package(url: "https://github.com/Kitura/Swift-JWT.git", from: "3.6.1")
+        .package(name: "SwiftJWT", url: "https://github.com/Kitura/Swift-JWT.git", from: "3.6.1")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "UpbitSwift",
-            dependencies: ["SwiftJWT"]),
+            dependencies: ["Alamofire", "SwiftJWT"],
+            path: "Sources"),
         .testTarget(
             name: "UpbitSwiftTests",
             dependencies: ["UpbitSwift"]),
